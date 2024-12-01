40ce1cd97202cd10a98444e9b466f143131174da
Sia Ghelichkhan <ghelichkhani.siavash@gmail.com>
2024-12-01 22:48:26 +1100

    Cleaning up models from Cammarano

--- examples/01_anelasticity_models.py
+++ examples/01_anelasticity_models.py
@@ -6,20 +6,20 @@ from gdrift.profile import RadialProfileSpline
 # In this tutorial we show how with the given functionalities
 # we can apply anelastic correction to an existing thermodynamic table
 
-# For our anelastic model in this example we use the study by Cammerano et al.
-# The anelastic parameterisation is dependent on a solidus curve of the mantle.
-# To this end we use the two datasets by Andrault et al 2011 EPSL
-# And a solidus by a study by Hirschmann to build a combined solidus curve.
+# In this example, we use the anelastic model from Cammarano et al.
+# The anelastic parameterization depends on the mantle's solidus curve.
+# We combine two datasets to build this curve:
+# - Andrault et al. (2011, EPSL)
+# - Hirschmann (2000, G3)
 
 
 def build_solidus():
-    # Defining the solidus curve for manlte
-    # Andrault et al 2011
+    # Load the solidus curve for the mantle from Andrault et al. (2011)
     andrault_solidus = gdrift.SolidusProfileFromFile(
         model_name="1d_solidus_Andrault_et_al_2011_EPSL",
-        description="Andrault et al 2011 EPSL")
+        description="Andrault et al. 2011, EPSL")
 
-    # Hirsch solidus
+    # Load the solidus curve for the mantle from Hirschmann (2000)
     hirsch_solidus = gdrift.HirschmannSolidus()
 
     # Combining the two
@@ -35,7 +35,7 @@ def build_solidus():
     my_depths.extend([3000e3])
     my_solidus.extend([solidus_model.at_depth(dpths[-1])])
 
-    # building the solidu profile that was originally used by Ghelichkhan et al in their study
+    # building the solidus profile that was originally used by Ghelichkhan et al. (2021)
     ghelichkhan_et_al = RadialProfileSpline(
         depth=np.asarray(my_depths),
         value=np.asarray(my_solidus),
@@ -45,47 +45,52 @@ def build_solidus():
 
 
 # Now the anelasticiy model by Cammarano et al can be constructed.
-# Here we use B g a for making Q_\mu. and we provide a rough Q_\kappa
-# Provided by Goes et al.
+# Here we use B g a for making Q_\mu, and we provide a rough Q_\kappa
+# From Goes et al, set to a constant value of 1000 in the upper and 10000 in the lower mantle.
 
-def build_anelasticity_model(solidus, N=3):
-    N = str(N)
-    Q = {
-        "1": {
+def build_anelasticity_model(solidus, q_profile: str = "Q1"):
+
+    cammarano_parameters = {
+        "Q1": {
             "B": [0.5, 10],
             "g": [20, 10]
         },
-        "2": {
+        "Q2": {
             "B": [0.8, 15],
             "g": [20, 10]
         },
-        "3": {
+        "Q3": {
             "B": [1.1, 20],
             "g": [20, 10]
         },
-        "4": {
+        "Q4": {
             "B": [0.035, 2.25],
             "g": [30, 15]
         },
-        "5": {
+        "Q5": {
             "B": [0.056, 3.6],
             "g": [30, 15]
         },
-        "6": {
+        "Q6": {
             "B": [0.077, 4.95],
             "g": [30, 15]
         }
     }
 
-    B = lambda x: np.where(x < 660e3, Q[N]["B"][0], Q[N]["B"][1])
+    def B(x):
+        return np.where(x < 660e3, cammarano_parameters[q_profile]["B"][0], cammarano_parameters[q_profile]["B"][1])
 
-    g = lambda x: np.where(x < 660e3, Q[N]["g"][0], Q[N]["g"][1])
+    def g(x):
+        return np.where(x < 660e3, cammarano_parameters[q_profile]["g"][0], cammarano_parameters[q_profile]["g"][1])
 
-    a = lambda x: 0.2
+    def a(x):
+        return 0.2
 
-    omega = lambda x: 1.
+    def omega(x):
+        return 1.
 
-    Q_kappa = lambda x: np.where(x < 660e3, 1e3, 1e4)
+    def Q_kappa(x):
+        return np.where(x < 660e3, 1e3, 1e4)
 
     return gdrift.CammaranoAnelasticityModel(B=B, g=g, a=a, solidus=solidus, Q_bulk=Q_kappa, omega=omega)
 
@@ -94,15 +99,17 @@ def build_anelasticity_model(solidus, N=3):
 prem = gdrift.PreliminaryRefEarthModel()
 
 # Thermodynamic model
-slb_pyrolite = gdrift.ThermodynamicModel("SLB_16", "pyrolite", temps=np.linspace(300, 4000), depths=np.linspace(0, 2890e3))
+slb_pyrolite = gdrift.ThermodynamicModel(
+    "SLB_16", "pyrolite", temps=np.linspace(300, 4000), depths=np.linspace(0, 2890e3))
 pyrolite_elastic_s_speed = slb_pyrolite.compute_swave_speed()
 pyrolite_elastic_p_speed = slb_pyrolite.compute_pwave_speed()
 
 # building solidus model
 solidus_ghelichkhan = build_solidus()
-N = 6 # choose model from cammarano et al., 2003
-anelasticity = build_anelasticity_model(solidus_ghelichkhan, 1)
-anelastic_slb_pyrolite = gdrift.apply_anelastic_correction(slb_pyrolite, anelasticity)
+cammarano_q_model = "Q1"  # choose model from cammarano et al., 2003
+anelasticity = build_anelasticity_model(solidus_ghelichkhan, q_profile=cammarano_q_model)
+anelastic_slb_pyrolite = gdrift.apply_anelastic_correction(
+    slb_pyrolite, anelasticity)
 pyrolite_anelastic_s_speed = anelastic_slb_pyrolite.compute_swave_speed()
 pyrolite_anelastic_p_speed = anelastic_slb_pyrolite.compute_pwave_speed()
 
@@ -163,7 +170,7 @@ ax_2.vlines(
 ax_2.set_xlabel("Temperature[K]")
 ax_2.set_ylabel("Shear Seismic-Wave Speed [m/s]")
 ax_2.text(
-    0.5, 1.05, s=f"cammarano et al. Q{N} at depth {pyrolite_anelastic_s_speed.get_x()[index] / 1e3:.1f} [km]",
+    0.5, 1.05, s=f"cammarano et al. {cammarano_q_model} at depth {pyrolite_anelastic_s_speed.get_x()[index] / 1e3:.1f} [km]",
     ha="center", va="center",
     transform=ax_2.transAxes, bbox=dict(facecolor=(1.0, 1.0, 0.7)))
 ax_2.legend()
@@ -190,7 +197,7 @@ ax_3.vlines(
 ax_3.set_xlabel("Temperature[K]")
 ax_3.set_ylabel("Compressional Seismic-Wave Speed [m/s]")
 ax_3.text(
-    0.5, 1.05, s=f"cammarano et al. Q{N} at depth {pyrolite_anelastic_p_speed.get_x()[index] / 1e3:.1f} [km]",
+    0.5, 1.05, s=f"cammarano et al. {cammarano_q_model} at depth {pyrolite_anelastic_p_speed.get_x()[index] / 1e3:.1f} [km]",
     ha="center", va="center",
     transform=ax_3.transAxes, bbox=dict(facecolor=(1.0, 1.0, 0.7)))
 ax_3.legend()
